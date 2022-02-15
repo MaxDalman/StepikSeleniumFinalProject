c80bcfbc5777a97e5084b9dee163fd9db40cf35a
MaxDalman <maksimdaubert@gmail.com.com>
2022-02-15 10:44:55 +0300

    Initializing the transition to the login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,7 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
