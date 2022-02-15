944225e65b1bee11d0335175c3330dc212d7a5d8
MaxDalman <maksimdaubert@gmail.com.com>
2022-02-15 10:23:18 +0300

    Adding test of navigatoin to the login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -12,3 +13,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open
+    page.should_be_login_url()
